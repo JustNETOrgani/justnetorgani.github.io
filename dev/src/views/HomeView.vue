046e74abe0001fd5b0dd7f759c7f08276401bcfb
justnetorgani <justnetonline@outlook.com>
2023-07-04 16:09:45 +0800

    Google material icons added

--- dev/src/views/HomeView.vue
+++ dev/src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <script setup>
-import HobLang from '../components/HobLang.vue'
 </script>
 
 <template>
@@ -10,29 +9,35 @@ import HobLang from '../components/HobLang.vue'
     <h2>Justice Odoom (PhD, MSE, BSc, BEd, Dip)</h2>
     <br>
     <div class="myBio">
-      <v-icon name="fa-user-circle" /><h3>Biography</h3>
+      <span class="material-icons">person</span>
+			<span class="text">Biography</span>
       <p>I received my BSc and MSE degrees in Computer Science from Data Link University, Tema, Ghana and Southwest University of Science and Technology, Mianyang-Sichuan, China, in 2015 and 2020 respectively. I obtained my PhD in 2023 from Southwest University of Science and Technology, China. 
           I am a certified Elsevier and Publons academy peer reviewer. My research interests include information security, blockchain technology, ring signatures and privacy-preservation in the sharing of Electronic Health Records (EHRs). 
           I am a member of IEEE and IEEE Computer Society. 
       </p>
     </div>
-    <div class="bottomMain">
-      <v-icon name="co-happy" /><h3>HOBBIES</h3>
-      <el-icon class="icon"><Reading /></el-icon>Reading tech journals
-      <br>
-      <v-icon class="icon" name="gi-grand-piano" />Playing the organ
-      <br>
-      <el-icon class="icon"><Film /></el-icon>Watching movies
-    </div>
-    <div class="bottomMain">
-      <h3>HOBBIES</h3>
-      <v-icon name="co-language" /><h3>LANGUAGES</h3>
+    <el-row>
+      <el-col :span="12"><div class="grid-content ep-bg-purple" />
+        <span class="material-icons">thumb_up_alt</span>
+			  <span class="text">HOBBIES</span>
+        <br>
+        <el-icon class="icon"><Reading /></el-icon>Reading tech journals
+        <br>
+        <v-icon class="icon" name="gi-grand-piano" />Playing the organ
+        <br>
+        <el-icon class="icon"><Film /></el-icon>Watching movies
+      </el-col>
+      <el-col :span="8"><div class="grid-content ep-bg-purple-light" />
+        <span class="material-icons">translate</span>
+			  <span class="text">LANGUAGES</span>
+        <br>
         <el-tag>English</el-tag>
         <el-tag class="ml-2" type="info">Chinese</el-tag>
         <br>
         <el-tag class="ml-2" type="warning">French</el-tag>
         <el-tag>Twi (GH lang.)</el-tag>
-    </div>
+      </el-col>
+    </el-row>
   </main>
 </template>
 
@@ -67,7 +72,8 @@ h3 {
   margin-bottom: 0.5rem;
 }
 
-.bottomMain {
-  width: 40%;
-}
+.icon {
+    margin-top: 1rem;
+  }
+
 </style>
