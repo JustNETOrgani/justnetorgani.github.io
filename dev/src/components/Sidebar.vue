dd3c21a229267f073a82be7a6566086b7adca575
justnetorgani <justnetonline@outlook.com>
2023-07-04 09:45:00 +0800

    Homepage created

--- dev/src/components/Sidebar.vue
+++ dev/src/components/Sidebar.vue
@@ -48,12 +48,12 @@ const ToggleMenu = () => {
 
 		<div class="flex"></div>
 		
-		<div class="menu">
+		<!-- <div class="menu">
 			<router-link to="/settings" class="button">
 				<span class="material-icons">settings</span>
 				<span class="text">Settings</span>
 			</router-link>
-		</div>
+		</div> -->
 	</aside>
 </template>
 
--- dev/src/views/HomeView.vue
+++ dev/src/views/HomeView.vue
@@ -3,7 +3,42 @@
 
 <template>
   <main class="home-page">
-    <h1>Home</h1>
-    <p>This is the home page </p>
+    <div class="imagediv">
+      <img alt="JusticeImg" class="mypic" src="@/assets/gitpagepic.jpg" />
+    </div>
+    <h2>Justice Odoom (PhD, MSE, BSc, BEd, Dip)</h2>
+    <br>
+    <div class="myBio">
+      <v-icon name="fa-user-circle" />Biography
+      <p>I received my BSc and MSE degrees in Computer Science from Data Link University, Tema, Ghana and Southwest University of Science and Technology, Mianyang-Sichuan, China, in 2015 and 2020 respectively. I obtained my PhD in 2023 from Southwest University of Science and Technology, China. 
+          I am a certified Elsevier and Publons academy peer reviewer. My research interests include information security, blockchain technology, ring signatures and privacy-preservation in the sharing of Electronic Health Records (EHRs). 
+          I am a member of IEEE and IEEE Computer Society. 
+      </p>
+    </div>
+    
   </main>
 </template>
+
+<style lang="scss" scoped>
+.imagediv {
+  width: 100%;
+  margin-top: -1rem;
+}
+img{
+  width: 250px; 
+  height: 250px;
+  display: block;
+  margin: auto;
+  border-radius: 50%;
+  border: 7px solid cadetblue;
+}
+
+h2 {
+  margin-top: 1rem;
+  text-align: center;
+}
+
+.myBio{
+  margin-top: 1rem;
+}
+</style>
