8c2944fa1ba6a470ad4f2f255ccea9aeb078bafc
justnetorgani <justnetonline@outlook.com>
2023-07-05 09:28:12 +0800

    Awards page done

--- dev/src/views/awards.vue
+++ dev/src/views/awards.vue
@@ -1,10 +1,45 @@
 <script setup>
+const tableData = [
+  {
+    award: 'Outstanding international students’ awards (Second prize)',
+    institution: 'Southwest University of Science and Technology, Mianyang, Sichuan-China',
+    year: '2022',
+  },
+  {
+    award: 'Publication award at school 70th anniversary celebration',
+    institution: 'Southwest University of Science and Technology, Mianyang, Sichuan-China',
+    year: '2022',
+  },
+  {
+    award: 'Innovation and entrepreneurship for digital economy',
+    institution: 'Tsinghua University, Beijing, China',
+    year: '2021'
+  }
+]
 </script>
 
 <template>
     <main class="awards-page">
-        <h1>Awards</h1>
-      <p>This is awards page</p>
+        <h2>Awards received</h2>
+        <div class="eduDiv">
+            <el-table :data="tableData" stripe style="width: 80%">
+                <el-table-column prop="award" label="Award" width="310" />
+                <el-table-column prop="institution" label="Institution" width="340" />
+                <el-table-column prop="year" label="Year" />
+            </el-table>
+        </div>
     </main>
 </template>
+
+<style lang="scss" scoped>
+h2 {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  text-align: center;
+}
+.eduDiv {
+    align-content: center;
+    margin-bottom: 2rem;
+}
+</style>
   
