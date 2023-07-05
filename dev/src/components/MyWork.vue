3d757eb9516a58e958ec9a7e73f17bc47f7f81c9
justnetorgani <justnetonline@outlook.com>
2023-07-05 11:06:47 +0800

    Work and service added

---
+++ dev/src/components/MyWork.vue
@@ -0,0 +1,44 @@
+<script setup>
+import ContactItem from './ContactItem.vue'
+</script>
+
+<template>
+    <ContactItem>
+    <template #icon>
+        
+    </template>
+    <template #heading>University name loading...</template>
+    <h4>Position: Lecturer</h4>
+  </ContactItem>
+
+  <ContactItem>
+    <template #icon>
+        <img alt="5GECImg" class="logoSchool" src="@/assets/5gec.png" />
+    </template>
+    <template #heading>Ghana Armed Forces Education Unit GHQ (5 Garrison Education), Burma Camp-Accra</template>
+    <h4>Position: ICT facilitator</h4>
+  </ContactItem>
+
+  <ContactItem>
+    <template #icon>
+        <img alt="GESImg" class="logoSchool" src="@/assets/ges.png" />
+    </template>
+    <template #heading>Presbyterian Education Unit</template>
+    <h4>Position: ICT/Mathematics/Science facilitator</h4>
+  </ContactItem>
+
+</template>
+
+<style lang="scss" scoped>
+  .icon {
+    margin-top: 1rem;
+  }
+
+  img{
+  width: 40px; 
+  height: 40px;
+  display: block;
+  margin: auto;
+  border-radius: 50%;
+}
+</style>
--- dev/src/views/service.vue
+++ dev/src/views/service.vue
@@ -1,10 +1,41 @@
 <script setup>
+import MyWork from '../components/MyWork.vue'
 </script>
 
 <template>
     <main class="service-page">
-        <h1>Service</h1>
-      <p>This is the service page</p>
+        <h2>Work</h2>
+            <MyWork />
+        <h2>Service</h2>
+        <h4>Journal reviews</h4>
+            <ul>
+                <li>Computer Science</li>
+            </ul>
+        <br>
+        <h4>Conference reviews</h4>
+            <ul>
+                <li>International Conference on Computer Application and Information Security (ICCAIS 2023)</li>
+                <li>International Conference on Privacy, Security & Trust (PST 2022)</li>
+            </ul> 
+            <br>
+        <h2>Professional Associations</h2>
+            <ul>
+                <li>Institute of Electrical and Electronics Engineers (IEEE)</li>
+                <li>IEEE Computer Society</li>
+            </ul>   
     </main>
 </template>
+
+<style lang="scss" scoped>
+h2 {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+li {
+    margin-top: 1rem;
+    font-style: italic;
+}
+</style>
   
