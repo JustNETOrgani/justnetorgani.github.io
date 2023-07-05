47194df33618022812aa012ca37d75de1e995e18
justnetorgani <justnetonline@outlook.com>
2023-07-05 11:14:23 +0800

    Newsfeed added

--- dev/src/views/newsfeed.vue
+++ dev/src/views/newsfeed.vue
@@ -1,10 +1,30 @@
 <script setup>
+import { ref } from 'vue'
+
+const loading = ref(true)
+
+const svg = `
+        <path class="path" d="
+          M 30 15
+          L 28 17
+          M 25.61 25.61
+          A 15 15, 0, 0, 1, 15 30
+          A 15 15, 0, 1, 1, 27.99 7.5
+          L 15 15
+        " style="stroke-width: 4px; fill: rgba(0, 0, 0, 0)"/>
+      `
 </script>
 
 <template>
     <main class="service-page">
         <h2>News flash</h2>
-        <p>This is news flash</p>
+        <div v-loading="loading"
+          element-loading-text="Loading..."
+          :element-loading-spinner="svg"
+          element-loading-svg-view-box="-10, -10, 50, 50"
+          element-loading-background="rgba(122, 122, 122, 0.8)">
+
+        </div>
     </main>
 </template>
 
