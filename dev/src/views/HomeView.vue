eb63bb9ba4d57127053059678deb5ae4e7baef5e
justnetorgani <justnetonline@outlook.com>
2023-07-08 17:41:56 +0800

    Added tech stack

--- dev/src/views/HomeView.vue
+++ dev/src/views/HomeView.vue
@@ -1,4 +1,31 @@
-<script setup>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const percentage = ref(20)
+const customColor = ref('#409eff')
+const customColors = [
+  { color: '#f56c6c', percentage: 20 },
+  { color: '#e6a23c', percentage: 40 },
+  { color: '#5cb87a', percentage: 60 },
+  { color: '#1989fa', percentage: 80 },
+  { color: '#6f7ad3', percentage: 100 },
+]
+
+const customColorMethod = (percentage: number) => {
+  if (percentage > 75) {
+    return '#409eff'
+  }
+  if (percentage < 75 && percentage > 60 ) {
+    return '#909399'
+  }
+  if (percentage < 60) {
+    return '#e6a23c'
+  }
+  if (percentage < 30) {
+    return '#909399'
+  }
+  return '#409eff'
+}
 </script>
 
 <template>
@@ -27,17 +54,153 @@
         <br>
         <el-icon class="icon"><Film /></el-icon>Watching movies
       </el-col>
-      <el-col :span="8"><div class="grid-content ep-bg-purple-light" />
+      <el-col :span="10"><div class="grid-content ep-bg-purple-light" />
         <span class="material-icons">translate</span>
 			  <span class="text">LANGUAGES</span>
         <br>
-        <el-tag>English</el-tag>
-        <el-tag class="ml-2" type="info">Chinese</el-tag>
-        <br>
-        <el-tag class="ml-2" type="warning">French</el-tag>
-        <el-tag>Twi (GH lang.)</el-tag>
+        <div class="progressDiv">
+          <el-progress type="circle" :percentage="95" :width="88" :color="customColorMethod">
+            <span class="percentage-label">English</span>
+          </el-progress>
+          <el-progress type="circle" :percentage="65" :width="88" :color="customColorMethod">
+              <span class="percentage-label">Chinese</span>
+          </el-progress>
+          <el-progress type="circle" :percentage="40" :width="88" :color="customColorMethod">
+              <span class="percentage-label">French</span>
+          </el-progress>
+          <el-progress type="circle" :percentage="100" :width="88" :color="customColorMethod">
+              <span class="percentage-label">Twi (GH lang.)</span>
+          </el-progress>
+        </div>
       </el-col>
     </el-row>
+    <div class="techStack">
+      <p class="tecStackText">TECHNOLOGY STACK</p>
+    <el-row class="smallGap">
+      <div class="holder"><p class="tecStackText">Programming languages</p></div>
+      <div class="holder"><p class="tecStackText">Databases</p></div>
+    </el-row>
+    <el-row class="smallGap">
+      <el-col :span="6"><div class="grid-content ep-bg-purple" />
+        <el-progress
+          :text-inside="true"
+          :stroke-width="20"
+          :percentage="90"
+          status="success"
+        >
+          <span>Vue.js</span>
+        </el-progress>
+      </el-col>
+      <el-col :span="6"><div class="grid-content ep-bg-purple" />
+        <el-progress
+          :text-inside="true"
+          :stroke-width="20"
+          :percentage="90"
+          status="success"
+        >
+          <span>Python/Django</span>
+        </el-progress>
+      </el-col>
+
+      <el-col :span="6"><div class="grid-content ep-bg-purple" />
+        <el-progress
+          :text-inside="true"
+          :stroke-width="20"
+          :percentage="90"
+          status="success"
+        >
+          <span>MySQL</span>
+        </el-progress>
+      </el-col>
+      <el-col :span="6"><div class="grid-content ep-bg-purple" />
+        <el-progress
+          :text-inside="true"
+          :stroke-width="20"
+          :percentage="90"
+          status="success"
+        >
+          <span>MongoDB</span>
+        </el-progress>
+      </el-col>
+    </el-row>
+    <el-row class="smallGap">
+      <el-col :span="6"><div class="grid-content ep-bg-purple" />
+        <el-progress
+          :text-inside="true"
+          :stroke-width="20"
+          :percentage="70"
+          :color="customColorMethod"
+        >
+          <span>React.js</span>
+        </el-progress>
+      </el-col>
+      <el-col :span="6"><div class="grid-content ep-bg-purple" />
+        <el-progress
+          :text-inside="true"
+          :stroke-width="20"
+          :percentage="85"
+          :color="customColorMethod"
+        >
+          <span>PHP</span>
+        </el-progress>
+      </el-col>
+
+      <el-col :span="6"><div class="grid-content ep-bg-purple" />
+        <el-progress
+          :text-inside="true"
+          :stroke-width="20"
+          :percentage="70"
+          :color="customColorMethod"
+        >
+          <span>BigChainDB</span>
+        </el-progress>
+      </el-col>
+    </el-row>
+    <el-row class="smallGap">
+      <el-col :span="6"><div class="grid-content ep-bg-purple" />
+        <el-progress
+          :text-inside="true"
+          :stroke-width="20"
+          :percentage="95"
+          status="success"
+        >
+          <span>Solidity</span>
+        </el-progress>
+      </el-col>
+      <el-col :span="6"><div class="grid-content ep-bg-purple" />
+        <el-progress
+          :text-inside="true"
+          :stroke-width="20"
+          :percentage="70"
+          :color="customColorMethod"
+        >
+          <span>Java</span>
+        </el-progress>
+      </el-col>
+    </el-row>
+    <el-row class="smallGap">
+      <el-col :span="6"><div class="grid-content ep-bg-purple" />
+        <el-progress
+          :text-inside="true"
+          :stroke-width="20"
+          :percentage="70"
+          :color="customColorMethod"
+        >
+          <span>C++</span>
+        </el-progress>
+      </el-col>
+      <el-col :span="6"><div class="grid-content ep-bg-purple" />
+        <el-progress
+          :text-inside="true"
+          :stroke-width="20"
+          :percentage="70"
+          :color="customColorMethod"
+        >
+          <span>Matlab</span>
+        </el-progress>
+      </el-col>
+    </el-row>
+    </div>
   </main>
 </template>
 
@@ -47,12 +210,12 @@
   margin-top: -1rem;
 }
 img{
-  width: 250px; 
-  height: 250px;
+  width: 210px; 
+  height: 210px;
   display: block;
   margin: auto;
   border-radius: 50%;
-  border: 7px solid cadetblue;
+  border: 7px solid rgb(121, 139, 139);
 }
 
 h2 {
@@ -76,4 +239,22 @@ h3 {
     margin-top: 1rem;
   }
 
+.progressDiv .el-progress--circle {
+  margin-right: 15px;
+}
+
+.holder{
+  width: 50%;
+}
+.techStack{
+  margin-top: 1rem;
+}
+
+.tecStackText {
+  text-align: center;
+}
+
+.smallGap{
+  margin-top: 4px;
+}
 </style>
