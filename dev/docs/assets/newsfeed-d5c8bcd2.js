2d0ac276d5dbf7aaaf5736d72aba5e155d3d61f4
justnetorgani <justnetonline@outlook.com>
2023-07-18 15:23:30 +0800

    Built for GitHub

---
+++ dev/docs/assets/newsfeed-d5c8bcd2.js
@@ -0,0 +1,10 @@
+import{_ as o,l as n,m as c,o as l,c as d,n as _,a as s,p as i,d as r}from"./index-c0137856.js";const p=e=>(i("data-v-980ebb38"),e=e(),r(),e),g={class:"service-page"},v=p(()=>s("h2",null,"News flash",-1)),u=`
+        <path class="path" d="
+          M 30 15
+          L 28 17
+          M 25.61 25.61
+          A 15 15, 0, 0, 1, 15 30
+          A 15 15, 0, 1, 1, 27.99 7.5
+          L 15 15
+        " style="stroke-width: 4px; fill: rgba(0, 0, 0, 0)"/>
+      `,h={__name:"newsfeed",setup(e){const t=n(!0);return(m,b)=>{const a=c("loading");return l(),d("main",g,[v,_(s("div",{"element-loading-text":"Loading...","element-loading-spinner":u,"element-loading-svg-view-box":"-10, -10, 50, 50","element-loading-background":"rgba(122, 122, 122, 0.8)"},null,512),[[a,t.value]])])}}},w=o(h,[["__scopeId","data-v-980ebb38"]]);export{w as default};
