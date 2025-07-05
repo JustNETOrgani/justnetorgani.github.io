614e765547ea0fa8a65c44ba060290f713935257
JustNet <justnetonline@outlook.com>
2025-07-05 13:14:17 +0800

    New services added

--- dev/src/views/service.vue
+++ dev/src/views/service.vue
@@ -9,19 +9,41 @@ import MyWork from '../components/MyWork.vue'
         <h2>Service</h2>
         <h4>Journal reviews</h4>
             <ul>
+                <li>Peer-to-Peer Networking and Applications</li>
+                <li>Scientific Reports</li>
+                <li>Cluster Computing, Springer Nature</li>
                 <li>Computer Science</li>
+                <li>Complex & Intelligent Systems, Springer Nature</li>
+                <li>Connection Science, Taylor & Francis</li>
+                <li>International Journal of Information Security, Springer Nature</li>
+                <li>Telecommunication Systems, Springer Nature</li>
+                <li>World Wide Web, Springer Nature</li>
+                <li>Artificial Intelligence Review, Springer</li>
             </ul>
         <br>
         <h4>Conference reviews</h4>
             <ul>
+                <li>International Conference on Computing, Networks and Internet of Things (CNIOT 2025)</li>
+                <li>International Conference on Computer Application and Information Security (ICCAIS 2024)</li>
                 <li>International Conference on Computer Application and Information Security (ICCAIS 2023)</li>
                 <li>International Conference on Privacy, Security & Trust (PST 2022)</li>
+            </ul> 
+            <br>
+        <h4>Invited Talks, Seminars and Conference Engagements</h4>
+            <ul>
+                <li>Member, Technical Program Committee: IEEE 6th International Conference on Computing, Networks and Internet of Things (CNIOT 2025)</li>
+                <li>Speaker: Bibliometric Analysis with Biblioshiny, Ghana Communication Technology University, August 14, 2024, Accra-Ghana</li>
+                <li>Member, Technical Program Committee: International Conference on Computer Application and Information Security (ICCAIS 2024)</li>
+                <li>Speaker: Manuscript preparation with LaTeX, Ghana Communication Technology University, March 21 & 28, 2024, Virtual event</li>
+                <li>Keynote Speaker: Securing IoT systems and networks with Blockchain Technology, International Conference on Artificial Intelligence, Systems and Network Security (AISNS), December 22-24, 2023, Mianyang, China</li>
+                <li>Member, Technical Program Committee: International Conference on Computer Application and Information Security (ICCAIS 2023)</li>
             </ul> 
             <br>
         <h2>Professional Associations</h2>
             <ul>
                 <li>Institute of Electrical and Electronics Engineers (IEEE)</li>
                 <li>IEEE Computer Society</li>
+                <li>IEEE Blockchain Community</li>
             </ul>   
     </main>
 </template>
