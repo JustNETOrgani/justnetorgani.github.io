a60b8e4a791d8288b0a418c09092b5b06a1c9acc
JustNet <justnetonline@outlook.com>
2025-07-05 15:09:56 +0800

    Updated publications and service pages

---
+++ dev/docs/assets/newsfeed-3aa270c5.js
@@ -0,0 +1,10 @@
+import{_ as n,j as l,k as c,o as d,c as _,l as i,a as s,p as r,d as p}from"./index-42d910c3.js";const t=e=>(r("data-v-0760498f"),e=e(),p(),e),g={class:"service-page"},v=t(()=>s("h2",null,"News flash",-1)),u=t(()=>s("div",null,null,-1)),f=`
+        <path class="path" d="
+          M 30 15
+          L 28 17
+          M 25.61 25.61
+          A 15 15, 0, 0, 1, 15 30
+          A 15 15, 0, 1, 1, 27.99 7.5
+          L 15 15
+        " style="stroke-width: 4px; fill: rgba(0, 0, 0, 0)"/>
+      `,h={__name:"newsfeed",setup(e){const a=l(!0);return(m,w)=>{const o=c("loading");return d(),_("main",g,[v,i(s("div",{"element-loading-text":"Loading...","element-loading-spinner":f,"element-loading-svg-view-box":"-10, -10, 50, 50","element-loading-background":"rgba(122, 122, 122, 0.8)"},null,512),[[o,a.value]]),u])}}},k=n(h,[["__scopeId","data-v-0760498f"]]);export{k as default};
